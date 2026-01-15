2da1ad39136bffa77043c5c39f924f70828e946d
michael6520 <mm6520590@gmail.com>
2026-01-14 18:43:43 -0700

    Optimizations

---
+++ helpers.cpp
@@ -0,0 +1,16 @@
+#include "helpers.h"
+
+void _____(_& __) {
+    if (__.____ < __.__.size()) {
+        size_t ________ = __.__[__.____].size();
+        __.___.push_back(static_cast<char>(________));
+        __.____++;
+        _____(__);
+    }
+}
+
+void ______(const _& __) {
+    for (const auto& __ : __.___) {
+        std::cout << __;
+    }
+}
---
+++ helpers.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <thread>
+#include <vector>
+
+struct _ {
+    std::vector<std::string> __;
+    std::vector<char> ___;
+    size_t ____ = 0;
+};
+
+void _____(_& __);
+
+void ______(const _& __);
